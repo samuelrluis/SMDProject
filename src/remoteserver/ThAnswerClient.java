760436e0009d7bbad583bd80a85f899fdf2eea58
samuelrluis <samuelrluis@gmail.com>
2016-12-16 23:22:44 +0000

    Few Changes
    Few Changes

--- src/remoteserver/ThAnswerClient.java
+++ src/remoteserver/ThAnswerClient.java
@@ -1,5 +1,8 @@
 package remoteserver;
 
+import common.Msg;
+import sun.plugin2.message.Message;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,33 +13,41 @@ import java.net.Socket;
 public class ThAnswerClient extends Thread {
     public static final int TIMEOUT = 5;
     public static final int MAX_SIZE = 4000;
-    ServerSocket socketToClient;
+    ServerSocket socketTCP;
+    Socket socketToClient;
     String msgClient;
+    private Msg msg;
 
-    ThAnswerClient(ServerSocket socketClient){
-        socketToClient=socketClient;
+    ThAnswerClient(ServerSocket socket){
+        socketTCP=socket;
     }
 
     @Override
     public void run() {
 
-        BufferedReader in;
+        ObjectInputStream in;
         OutputStream out;
         byte []fileChunck = new byte[MAX_SIZE];
 
         try{
+            socketToClient = socketTCP.accept();
             socketToClient.setSoTimeout(1000*TIMEOUT);
 
-/*            in = new BufferedReader(new InputStreamReader(socketToClient.getInputStream()));
-            out = socketToClient.getOutputStream();*/
+            in = new ObjectInputStream(socketToClient.getInputStream());
+           // in = new BufferedReader(new InputStreamReader(socketToClient.getInputStream()));
+            //out = socketToClient.getOutputStream();
 
-            //msgClient = in.readLine();
+            msg = (Msg) in.readObject();
+
+            System.out.println("recebi mensagem de" + msg.gethBeat().getName());
 
             System.out.println("Recebido: " + msgClient);
             System.out.println("thread concluida");
 
         } catch(IOException e){
             System.out.println("Ocorreu a excepcao de E/S: \n\t" + e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
 
         try{
