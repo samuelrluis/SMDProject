b8fd6c29cf7485d94426eaf90b93eef011d551ef
Diogo Santos <diogom_branco@hotmail.com>
2017-01-02 02:21:06 +0000

    não percebo pq a mensgem do cmd registo não segue

--- src/common/Msg.java
+++ src/common/Msg.java
@@ -23,4 +23,8 @@ public class Msg implements Serializable {
     public String getCommand() {
         return command;
     }
+
+    public void setCommand(String command){
+        this.command = command;
+    }
 }
